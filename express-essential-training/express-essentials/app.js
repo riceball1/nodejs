5f8eaac03557d4b6ff9b528b74ead32187c04872
Dana Ng <ngdana1@gmail.com>
2023-02-05 11:30:49 -0800

    updated

--- express-essential-training/express-essentials/app.js
+++ express-essential-training/express-essentials/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import data from './data/mock.json' assert { type: "json" };
 
+const route = express.Router();
 const app = express();
 const PORT = 3000;
 
@@ -9,13 +10,13 @@ const PORT = 3000;
 app.use(express.static('public'))
 
 // json() and urlencoded()
-app.use(express.json()) 
+// app.use(express.json()) 
 // app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) => {
-    res.json(data)
-})
+    res.send('hello world!')
+  })
 
 // get with next
 app.get('/next', (req, res, next) => {
@@ -27,12 +28,22 @@ app.get('/next', (req, res, next) => {
 
 
 // get contact
-app.route('/contact')
-.get('/:contactId', (req, res) => {
+app
+.get('/contact/:contactId', (req, res) => {
     const id = Number(req.params.contactId);
     const contact = data.filter(contact => contact.id === id)
     res.send(contact)
 })
+
+app.get('/error', (req, res) => {
+    throw new Error('Error happened!')
+})
+
+// error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
