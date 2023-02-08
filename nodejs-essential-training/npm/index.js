c540ffce31d4aa6e0e33df9c949724f1ed2b7906
Dana Ng <ngdana1@gmail.com>
2023-02-08 11:42:12 -0800

    update npm

--- nodejs-essential-training/npm/index.js
+++ nodejs-essential-training/npm/index.js
@@ -1,6 +1,7 @@
 // parsing args
 
 import parseArgs from "minimist";
+import Timer from 'tiny-timer';
 
 const {time} = parseArgs(process.argv);
 
@@ -9,8 +10,15 @@ if (!time) {
     throw new Error("--time is required")
 }
 
+const timer = new Timer();
+
+timer.on("tick", () => console.log("tick"))
+timer.on("done", () => console.log("ticking completed!"))
+
+timer.start(5000)
+
 if (!parseInt(time)) {
     throw new Error("--time must a number")
 }
 
-console.log(time)
+// console.log(time)
